3f870874e0ddb2fcdc752ce976a32e25b7508c55
Sam Ennis <sam.c.ennis@gmail.com>
2015-09-20 02:34:57 -0500

    Changed ID generator from 8 to 10 digits.

--- The Occupancy Project/public/js/utils.js
+++ The Occupancy Project/public/js/utils.js
@@ -1,4 +1,10 @@
-function generate8DigitID() {
-    return (Math.random().toString(16) + "00000000").substr(2, 8);
+function generate10DigitID() {
+    
+    function _p8(s){
+        var p = (Math.random().toString(16) + "00000000").substr(2, 8);
+        return s ? p.substr(0,2) : p ;
+    }
+    
+    return _p8() + _p8(true); 
 }     
 
